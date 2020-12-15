5b1740d23450cc12ec1694bbe95ab801dd13b5bc
Jithendra <jithendrads@gmail.com>
2020-12-15 15:36:03 +0530

    Lamda Expression with Collections

--- Java8Features/src/com/jithendra/LambdaWithCollection.java
+++ Java8Features/src/com/jithendra/LambdaWithCollection.java
@@ -3,6 +3,8 @@ package com.jithendra;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /*class MyComparator implements Comparator<Integer>
 {
@@ -28,15 +30,18 @@ public class LambdaWithCollection {
 	public static void main(String[] args) {
 		ArrayList<Integer> l=new ArrayList<Integer>();
 		l.add(20);
-		l.add(10);
+		l.add(5);
 		l.add(30);
-		l.add(50);
+		l.add(55);
 		l.add(40);
 		System.out.println(l);
 		//Collections.sort(l);
 		Comparator<Integer> c=(i1,i2)->(i1<i2)?-1:(i1>i2)?1:0;
 		Collections.sort(l,c);
 		System.out.println(l);
+		l.stream().forEach(System.out::println);
+		List<Integer> l2=l.stream().filter(a->a%2==0).collect(Collectors.toList());
+		System.out.println(l2);
 
 	}
 
