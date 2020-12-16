ecae7c330a72b5530a5d6e8e51c7939466b155b4
Jithendra <jithendrads@gmail.com>
2020-12-16 12:58:11 +0530

    Multiple Inheritance concept using default methods in Interface

--- Java8Features/src/com/jithendra/MultipleInheritanceusingDefaultMethods.java
+++ Java8Features/src/com/jithendra/MultipleInheritanceusingDefaultMethods.java
@@ -4,6 +4,7 @@ package com.jithendra;
 
 interface Left
 {
+	void abstractmethod1();
 	default void m1()
 	{
 		System.out.println("m1 method in interface one");
@@ -13,6 +14,7 @@ interface Left
 
 interface Right
 {
+	void abstractmethod1();
 	default void m1()
 	{
 		System.out.println("m1 method in interface two");
@@ -24,15 +26,26 @@ public class MultipleInheritanceusingDefaultMethods implements Left,Right{
 	
 	public void m1()
 	{
-		//System.out.println("Overriding default method");
-		//But if you want I1's m1() method implementation then
+		//if you want to override default method
+		System.out.println("Overriding default method");
+		//But if you want Left's m1() method implementation then
 		Left.super.m1();
+		
+		//But if you want Right's m1() method implementation then
+		Right.super.m1();
 	}
 	
 	public static void main(String[] args)
 	{
 		MultipleInheritanceusingDefaultMethods d =new MultipleInheritanceusingDefaultMethods();
 		d.m1();
+		d.abstractmethod1();
+	}
+
+	@Override
+	public void abstractmethod1() {
+		System.out.println("overriding abstract method");
+		
 	}
 
 }
