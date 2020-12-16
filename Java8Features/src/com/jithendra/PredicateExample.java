f2da08a21a0b8252f206da899cef96c452de665b
Jithendra <jithendrads@gmail.com>
2020-12-16 18:34:51 +0530

    Predicate Example

---
+++ Java8Features/src/com/jithendra/PredicateExample.java
@@ -0,0 +1,51 @@
+package com.jithendra;
+
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+class EmployeeExample
+{
+	public String name;
+	public int salary;
+	
+	EmployeeExample(String name,int salary)
+	{
+		this.name=name;
+		this.salary=salary;
+	}
+}
+
+public class PredicateExample {
+
+	public static void main(String[] args) {
+		/*Predicate<Integer> p1=i->i%2==0;
+		System.out.println(p1.test(10));
+		System.out.println(p1.test(15));
+		Predicate<String> p2=s->s.length()%2==0;
+		System.out.println(p2.test("jithendra"));
+		System.out.println("---------------------------------------------");
+		String[] s= {"Shewag","Dravid","Dhoni","anil kumble","Irfan patan"};
+		for(String temp : s)
+		{
+			if(p2.test(temp))
+				System.out.println(temp);
+		}
+			*/
+		
+		ArrayList<EmployeeExample> l=new ArrayList<EmployeeExample>();
+		l.add(new EmployeeExample("Shewag",1234));
+		l.add(new EmployeeExample("Dravid",9876));
+		l.add(new EmployeeExample("Dhoni",2345));
+		l.add(new EmployeeExample("anil kumble",8765));
+		l.add(new EmployeeExample("Irfan patan",3456));
+		Predicate<EmployeeExample> p1=e->e.salary>3000;
+		for(EmployeeExample e:l)
+		{
+			if(p1.test(e))
+				System.out.println(e.name+" "+e.salary);
+		}
+		
+
+	}
+
+}
